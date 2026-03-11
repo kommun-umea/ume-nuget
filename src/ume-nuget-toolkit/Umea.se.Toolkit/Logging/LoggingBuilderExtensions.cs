4da85ecfff4fadde95532d7cbee51b8a247d673c
Simon Johansson <simon.johansson@umea.se>
2026-03-11 08:49:15 +0000

    Merged PR 3244: Added user tracing to cloud logger
    - Added cloud log tracing
    - Converted extension methods to extension blocks
    
    Related work items: #16751

--- src/ume-nuget-toolkit/Umea.se.Toolkit/Logging/LoggingBuilderExtensions.cs
+++ src/ume-nuget-toolkit/Umea.se.Toolkit/Logging/LoggingBuilderExtensions.cs
@@ -3,6 +3,8 @@ using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using OpenTelemetry.Trace;
 using Umea.se.Toolkit.Configuration;
 using Umea.se.Toolkit.Logging.ConsoleLogger;
 using Umea.se.Toolkit.Logging.OnPremLogger;
@@ -11,53 +13,79 @@ namespace Umea.se.Toolkit.Logging;
 
 internal static class LoggingBuilderExtensions
 {
-    internal static ILoggingBuilder RemoveLoggers(this ILoggingBuilder builder)
+    extension(ILoggingBuilder builder)
     {
-        return builder.ClearProviders();
-    }
+        internal ILoggingBuilder RemoveLoggers()
+        {
+            return builder.ClearProviders();
+        }
 
-    internal static ILoggingBuilder AddCustomConsoleLogger(this ILoggingBuilder builder)
-    {
-        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, ConsoleLoggerProvider>());
+        internal ILoggingBuilder AddCustomConsoleLogger()
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, ConsoleLoggerProvider>());
 
-        return builder;
-    }
+            return builder;
+        }
 
-    internal static ILoggingBuilder AddApplicationInsightsLogger(this ILoggingBuilder builder, ApplicationConfigFunctionsBase config)
-    {
-        builder.Services
-            .AddOpenTelemetry()
-            .UseAzureMonitorExporter(options =>
-            {
-                options.ConnectionString = config.ApplicationInsightsConnectionString;
-            });
+        internal ILoggingBuilder AddApplicationInsightsLogger(ApplicationConfigFunctionsBase config)
+        {
+            builder.Services
+                .AddOpenTelemetry()
+                .UseAzureMonitorExporter(options =>
+                {
+                    options.ConnectionString = config.ApplicationInsightsConnectionString;
+                });
 
-        return builder;
-    }
+            return builder;
+        }
 
-    internal static ILoggingBuilder AddApplicationInsightsLogger(this ILoggingBuilder builder, ApplicationConfigCloudBase config)
-    {
-        builder.Services
-            .AddOpenTelemetry()
-            .UseAzureMonitor(options =>
-            {
-                options.ConnectionString = config.ApplicationInsightsConnectionString;
-            });
+        internal ILoggingBuilder AddApplicationInsightsLogger(ApplicationConfigCloudBase config)
+        {
+            builder.Services
+                .AddOpenTelemetry()
+                .UseAzureMonitor(options =>
+                {
+                    options.ConnectionString = config.ApplicationInsightsConnectionString;
+                })
+                .WithTracing(tracing =>
+                {
+                    tracing.AddAspNetCoreInstrumentation(options =>
+                    {
+                        options.EnrichWithHttpRequest = (activity, httpRequest) =>
+                        {
+                            if (httpRequest.Headers.TryGetValue("Referer", out StringValues referer))
+                            {
+                                activity.SetTag("Referer", new[] { referer.ToString() });
+                            }
 
-        return builder;
-    }
+                            if (httpRequest.Headers.TryGetValue("User-Agent", out StringValues userAgent))
+                            {
+                                activity.SetTag("User-Agent", userAgent.ToString());
+                            }
 
-    internal static ILoggingBuilder AddOnPremLogger(this ILoggingBuilder builder, ApplicationConfigOnPremBase config)
-    {
-        builder.Services
-            .AddHttpClient(nameof(OnPremLogger.OnPremLogger), httpClient =>
-            {
-                httpClient.BaseAddress = new Uri(config.OnPremLoggerUrl);
-                httpClient.DefaultRequestHeaders.Add("X-Api-Key", config.OnPremLoggerKey);
-            });
+                            if (httpRequest.Headers.TryGetValue("X-AuthenticatedUserId", out StringValues authenticatedUserId))
+                            {
+                                activity.SetTag("enduser.id", authenticatedUserId.ToString());
+                            }
+                        };
+                    });
+                });
 
-        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, OnPremLoggerProvider>());
+            return builder;
+        }
 
-        return builder;
+        internal ILoggingBuilder AddOnPremLogger(ApplicationConfigOnPremBase config)
+        {
+            builder.Services
+                .AddHttpClient(nameof(OnPremLogger.OnPremLogger), httpClient =>
+                {
+                    httpClient.BaseAddress = new Uri(config.OnPremLoggerUrl);
+                    httpClient.DefaultRequestHeaders.Add("X-Api-Key", config.OnPremLoggerKey);
+                });
+
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, OnPremLoggerProvider>());
+
+            return builder;
+        }
     }
 }
